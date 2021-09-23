674a8adffa5d063f87cd06d4a4f119cf5b83d9ec
Wilson Sous <souswilson@gmail.comgit config --list>
2021-09-23 04:21:12 -0400

    updated files

---
+++ public/assets/js/index.js
@@ -0,0 +1,151 @@
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNoteBtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
+
+// Get function from the db
+const getNotes = () => {
+  return $.ajax({
+    url: "/api/notes",
+    method: "GET",
+  });
+};
+
+// POST function to the db
+const saveNote = (note) => {
+  return $.ajax({
+    url: "/api/notes",
+    data: note,
+    method: "POST",
+  });
+};
+
+// Delete function from the db
+const deleteNote = (id) => {
+  return $.ajax({
+    url: "api/notes/" + id,
+    method: "DELETE",
+  });
+};
+
+// If there is an activeNote, display it, otherwise render empty inputs
+const renderNote = () => {
+  $saveNoteBtn.hide();
+
+  if (activeNote.id != null && activeNote.id !== undefined) {
+    $noteTitle.attr("readonly", true);
+    $noteText.attr("readonly", true);
+    $noteTitle.val(activeNote.title);
+    $noteText.val(activeNote.text);
+  } else {
+    $noteTitle.attr("readonly", false);
+    $noteText.attr("readonly", false);
+    $noteTitle.val("");
+    $noteText.val("");
+  }
+};
+
+// Get the note data from the inputs, save it to the db and update the view
+const noteSave = function () {
+  const newNote = {
+    title: $noteTitle.val(),
+    text: $noteText.val(),
+  };
+
+  saveNote(newNote).then(() => {
+    getAndRenderNotes();
+    renderNote();
+  });
+};
+
+// Delete the clicked note
+const noteDelete = function (event) {
+  // prevents the click listener for the list from being called when the button inside of it is clicked
+  event.stopPropagation();
+
+  const note = $(this).parent(".list-group-item").data();
+
+  if (activeNote.id === note.id) {
+    activeNote = {};
+  }
+
+  deleteNote(note.id).then(() => {
+    getAndRenderNotes();
+    renderNote();
+  });
+};
+
+// Sets the activeNote and displays it
+const handleNoteView = function () {
+  activeNote = $(this).data();
+  renderNote();
+};
+
+// Sets the activeNote to and empty object and allows the user to enter a new note
+const viewNote = function () {
+  activeNote = {};
+  renderNote();
+};
+
+// If a note's title or text are empty, hide the save button
+// Or else show it
+const saveBtn = function () {
+  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+    $saveNoteBtn.hide();
+  } else {
+    $saveNoteBtn.show();
+  }
+};
+
+// Render's the list of note titles
+const renderNoteList = (notes) => {
+  $noteList.empty();
+
+  const noteListItems = [];
+
+  // Returns jquery object for li with given text and delete button
+  // unless withDeleteButton argument is provided as false
+  const create$li = (text, withDeleteButton = true) => {
+    const $li = $("<li class='list-group-item'>");
+    const $span = $("<span>").text(text);
+    $li.append($span);
+
+    if (withDeleteButton) {
+      const $delBtn = $(
+        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      );
+      $li.append($delBtn);
+    }
+    return $li;
+  };
+
+  if (notes.length === 0) {
+    noteListItems.push(create$li("No saved Notes", false));
+  }
+
+  notes.forEach((note) => {
+    const $li = create$li(note.title).data(note);
+    noteListItems.push($li);
+  });
+
+  $noteList.append(noteListItems);
+};
+
+// Gets notes from the db and renders them to the sidebar
+const getAndRenderNotes = () => {
+  return getNotes().then(renderNoteList);
+};
+
+$saveNoteBtn.on("click", noteSave);
+$noteList.on("click", ".list-group-item", handleNoteView);
+$newNoteBtn.on("click", viewNote);
+$noteList.on("click", ".delete-note", noteDelete);
+$noteTitle.on("keyup", saveBtn);
+$noteText.on("keyup", saveBtn);
+
+// Gets and renders the initial list of notes
+getAndRenderNotes();
---
+++ server.js
@@ -0,0 +1,102 @@
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// EXPRESS CONFIGURATION
+
+var app = express();
+
+// Sets an initial port.
+var PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// ROUTER
+// The below points our server to a series of "route" files.
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+// Post Route
+app.post("/api/notes", function (req, res) {
+  let savedNotes = [];
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+
+    const noteData = JSON.parse(data);
+
+    for (let i = 0; i < noteData.length; i++) {
+      const note = {
+        title: noteData[i].title,
+        text: noteData[i].text,
+        id: i,
+      };
+
+      savedNotes.push(note);
+    }
+
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: savedNotes.length,
+    };
+
+    savedNotes.push(newNote);
+
+    savedNotes = JSON.stringify(savedNotes);
+
+    fs.writeFile("./db/db.json", savedNotes, (err) => {
+      if (err) throw err;
+      console.log("File saved successfully.");
+    });
+
+    res.send("Note added successfully.");
+  });
+});
+
+// Delete Route
+app.delete("/api/notes/:id", function (req, res) {
+  const noteID = req.params.id;
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+
+    const noteData = JSON.parse(data);
+    for (let i = 0; i < noteData.length; i++) {
+      if (noteData[i].id === parseInt(noteID)) {
+        noteData.splice(i, 1);
+        break;
+      }
+    }
+
+    updatedData = JSON.stringify(noteData);
+
+    // Write data back to stored db.json
+    fs.writeFile("./db/db.json", updatedData, (err) => {
+      if (err) throw err;
+    });
+
+    res.send("Note deleted successfully.");
+  });
+});
+
+// Routes for HTML
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
